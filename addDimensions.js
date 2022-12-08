0271346e5e3fe48aea3d4d61370fc567a7a4c409
Jeremiah Blanch <jblanch@nrl.com.au>
2022-12-08 15:36:35 +1100

    rewrite to match new API, use regex more efficiently

--- addDimensions.js
+++ addDimensions.js
@@ -1,12 +1,12 @@
 'use strict';
 
 exports.name = 'addDimensions';
-exports.description = 'Adds width and height accordiong to viewBox';
+exports.description = 'Adds width and height according to viewBox';
 exports.active = true;
 exports.type = 'perItem';
 
 /**
- * Remove width/height attributes when a viewBox attribute is present.
+ * Add width/height attributes based on viewBox values, if they aren't already present
  *
  * @example
  * <svg viewBox="0 0 100 50">
@@ -14,47 +14,32 @@ exports.type = 'perItem';
  * <svg width="100" height="50" viewBox="0 0 100 50">
  *
  * @param {Object} item current iteration item
- * @return {Boolean} if true, with and height will be filtered out
+ * @return {Boolean} if true, witdh and height will be added
  *
- * @author sscherbin
+ * @author jeremiahblanch
  */
-exports.fn = function(svg) {
-    var viewBox;
-    var minX;
-    var minY;
-    var width;
-    var height;
 
-    if (
-    	!svg.isElem('svg') ||
-        !svg.hasAttr('viewBox') ||
-        svg.hasAttr('width') ||
-        svg.hasAttr('height')
-    ) {
-        return;
-    }
+const rx = /(\d*) (\d*) (\d*) (\d*)/;
 
-    viewBox = svg.attr('viewBox').value;
-    minX = parseFloat(viewBox.split(' ')[0]);
-    minY = parseFloat(viewBox.split(' ')[1]);
-    width = parseFloat(viewBox.split(' ')[2]);
-    height = parseFloat(viewBox.split(' ')[3]);
-
-    if (width <= 0 || height <= 0) {
-    	return;
-    }
-
-    svg.addAttr({
-        name: 'width',
-        value: (width - minX) + 'px',
-        prefix: '',
-        local: 'width'
-    });
-
-    svg.addAttr({
-        name: 'height',
-        value: (height - minY) + 'px',
-        prefix: '',
-        local: 'height'
-    });
+exports.fn = () => {
+  return {
+    element: {
+      enter: (node, parentNode) => {
+        if (
+          node.name === 'svg' &&
+          parentNode.type !== 'root' &&
+          node.attributes.height == null &&
+          node.attributes.width == null &&
+          node.attributes.viewBox != null &&
+          rx.test(node.attributes.viewBox)
+        ) {
+          
+          const [_, x, y, w, h] = rx.exec(node.attributes.viewBox).map((v, index) => index > 0 ? parseFloat(v) : '');
+          
+          node.attributes.height = (h - y);
+          node.attributes.width = (w - x);
+        }
+      },
+    },
+  };
 };
